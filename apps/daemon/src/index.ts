a7097902c66ae5353b5444668608cfbcaec4c000
Emad Karimi <82148680+Emad211@users.noreply.github.com>
2026-07-22 16:36:01 +0330

    Export OL-011 finalization boundary

--- apps/daemon/src/index.ts
+++ apps/daemon/src/index.ts
@@ -1,6 +1,7 @@
 import { APP_NAME } from "@ownloop/contracts";
 
 export * from "./artifact-store/index.js";
+export * from "./finalization/index.js";
 export * from "./git-baseline/index.js";
 export * from "./git-reconciliation/index.js";
 export * from "./ingress/index.js";
